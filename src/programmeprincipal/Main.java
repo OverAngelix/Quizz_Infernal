c37d282995be8eee81cc6161401f6c63539faeaf
OverA <OverA@DESKTOP-OHS1QQP>
2018-06-12 00:01:31 +0200

    Inspecteur

--- src/programmeprincipal/Main.java
+++ src/programmeprincipal/Main.java
@@ -1,12 +1,13 @@
 package programmeprincipal;
 
 import javafx.application.Application;
-
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import phases.phase1;
 
 public class Main extends Application {
 
@@ -14,6 +15,18 @@ public class Main extends Application {
 	public void start(Stage stage) throws Exception {
 		Label accueil = new Label("Bienvenue sur ce jeu magnifique !");
 		Button start = new Button("NOUVELLE PARTIE !");
+		
+		start.addEventHandler(ActionEvent.ACTION, e -> {
+			phase1 p1 = new phase1();
+			Stage s = new Stage();
+			try {
+				p1.start(s);
+				stage.close();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		
 		
 		VBox root = new VBox(accueil,start);
 		root.setId("pane");
