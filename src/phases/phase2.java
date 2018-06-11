1e245ee0c7b7bd799cc363c3f8cf1fa654b568ba
OverA <OverA@DESKTOP-OHS1QQP>
2018-06-11 23:13:11 +0200

    HEYHYE

--- src/phases/phase2.java
+++ src/phases/phase2.java
@@ -1,5 +1,31 @@
 package phases;
 
-public class phase2 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class phase2 extends Application {
+
+	
+	public void start(Stage stage) throws Exception {
+		Label accueil = new Label("Bienvenue sur ce jeu magnifique !");
+		Button start = new Button("NOUVELLE PARTIE !");
+		
+		VBox root = new VBox(accueil,start);
+		root.setId("pane");
+		Scene scn = new Scene(root,400,400);
+		scn.getStylesheets().add(getClass().getResource("../css/main.css").toExternalForm());
+		stage.setScene(scn);
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
 }
--- src/programmeprincipal/Main.java
+++ src/programmeprincipal/Main.java
@@ -1,10 +1,32 @@
 package programmeprincipal;
 
-public class Main {
+import javafx.application.Application;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	
+	public void start(Stage stage) throws Exception {
+		Label accueil = new Label("Bienvenue sur ce jeu magnifique !");
+		Button start = new Button("NOUVELLE PARTIE !");
+		
+		VBox root = new VBox(accueil,start);
+		root.setId("pane");
+		Scene scn = new Scene(root,400,400);
+		scn.getStylesheets().add(getClass().getResource("../css/main.css").toExternalForm());
+		stage.setScene(scn);
+		stage.show();
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
+	
 }
